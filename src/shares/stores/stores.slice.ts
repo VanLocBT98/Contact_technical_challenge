023fe2d7db722e5062550b997c1761e166225404
Leooo <vanlocbtsl123@gmail.com>
2025-03-19 16:46:15 +0700

    fix(:hammer:): fix text

--- src/shares/stores/stores.slice.ts
+++ src/shares/stores/stores.slice.ts
@@ -1,15 +1,12 @@
 import { StoreApi, create } from 'zustand';
 
 import { AuthState, createAuthStore } from './auth.slice';
-import { ThemeState, createThemeStore } from './theme.slice';
 
 export type AppStore = {
   auth: AuthState;
-  theme: ThemeState;
 };
 
 export const createAppStore = (): StoreApi<AppStore> =>
   create<AppStore>()((set) => ({
-    auth: createAuthStore(set),
-    theme: createThemeStore(set)
+    auth: createAuthStore(set)
   }));
